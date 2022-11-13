f75506b287b0b2f0971e38ca3997335b92dd46b1
Vansansman <mamintargetolog@gmail.com>
2022-11-13 18:22:14 +0300

    unit 7 homework

---
+++ tests/test_user_registration.py
@@ -0,0 +1,39 @@
+from demo_qa_tests.model.pages import registration_form
+
+
+def test_submit_student_details(open_and_quit_browser_automation_practice_form):
+    registration_form.set_first_name('Никита')
+    registration_form.set_last_name('Кузнецов')
+    registration_form.set_email('mamintargetolog@gmail.com')
+    registration_form.set_gender('Male')
+    registration_form.set_phone_number('9111111111')
+    registration_form.set_date_of_birth([19, 'November', '1991'])
+    registration_form.set_subject('English')
+    registration_form.set_hobby('Music')
+    registration_form.set_picture('resources/picture.jpg')
+    registration_form.set_address('Russia, Moscow')
+
+    registration_form.scroll_to_bottom()
+    registration_form.set_state('Haryana')
+    registration_form.set_city('Karnal')
+
+    registration_form.click_submit()
+
+    registration_form.should_have_submitted(
+            [
+                ('Student Name', 'Никита Кузнецов'),
+                ('Student Email', 'mamintargetolog@gmail.com'),
+                ('Gender', 'Male'),
+                ('Mobile', '9111111111'),
+                ('Date of Birth', '19 November,1991'),
+                ('Subjects', 'English'),
+                ('Hobbies', 'Music'),
+                ('Picture', 'picture.jpg'),
+                ('Address', 'Russia, Moscow'),
+                ('State and City', 'Haryana Karnal')
+            ],
+        )
+
+
+
+
